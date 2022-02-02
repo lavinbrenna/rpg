1f88919ec037e3b57e9fe44417b59f7a40b3cca0
Brenna Lavin <lavinbrenna@gmail.com>
2022-02-02 10:56:46 -0800

    add player add method, add enemy add method
    Co-Authored-By: Ryan Bass <theryanbass@gmail.com>

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,16 +1,34 @@
 /* eslint-disable no-undef */
-import Game from './../src/js/game.js';
+import {Game, Player, Enemy} from './../src/js/game.js';
 
 describe('Game', () => {
 
-  let game;
+  let enemy, game, player;
 
   beforeEach(() => {
+    enemy = new Enemy();
     game = new Game();
+    player = new Player();
   });
 
   it('should create an empty game object', () =>{
     expect(game.players).toEqual({});
     expect(game.enemies).toEqual({});
+  });
+  it('should create player in game object', () =>{
+    expect(game.createPlayer()).toEqual(player);
+  });
+  it('should create enemy in game object', () =>{
+    expect(game.createEnemy()).toEqual(enemy);
+  });
+  it('should assign playerId, playerId = 1', () =>{
+    expect(game.assignPlayerId()).toEqual(1);
+  });
+  it('should assign enemyId, enemyId = 1', () =>{
+    expect(game.assignEnemyId()).toEqual(1);
+  });
+  it('should add player to players', () =>{
+    game.addPlayer(player);
+    expect(game.players[1]).toEqual(player);
   });
 });
--- src/js/game.js
+++ src/js/game.js
@@ -1,9 +1,51 @@
 import Player from './player.js';
 import Enemy from './enemy.js';
 
-export default class Game{
+export class Game {
   constructor(){
     this.players = {};
     this.enemies = {};
+    this.playerId = 0;
+    this.enemyId = 0;
   }
+  createPlayer(){
+    let player = new Player();
+    console.log(player);
+    return player;
+  }
+  assignPlayerId (){
+    this.playerId +=1;
+    return this.playerId;
+  }
+  createEnemy(){
+    let enemy = new Enemy();
+    return enemy;
+  }
+  assignEnemyId (){
+    this.enemyId +=1;
+    return this.enemyId;
+  }
+  addPlayer(player){
+    player.id = this.assignPlayerId();
+    this.players[player.id] = player;
+  }
+
 }
+export {Player, Enemy};
+
+/*
+
+
+addPlayer(player){
+  player.id = this.assignPlayerId();
+  this.players[player.id] = player;
+}
+
+
+
+*/
+
+// addPlayer(){
+//   let player = new Player;
+//   this.players.firstPlayer = player;
+// }
